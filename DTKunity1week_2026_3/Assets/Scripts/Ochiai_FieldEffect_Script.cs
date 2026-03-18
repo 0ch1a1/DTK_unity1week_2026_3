b7c1fcba2cd82f0b353a3f9d1e10ba80f61b5aed
0ch1a1@DTK <taishi.842aka@icloud.com>
2026-03-19 02:03:59 +0900

    ItemMove&ItemEffect, SplinesImport

---
+++ DTKunity1week_2026_3/Assets/Scripts/Ochiai_FieldEffect_Script.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Ochiai_FieldEffect_Script : MonoBehaviour
+{
+    [SerializeField] private  FieldType currentType;
+    [SerializeField] private float DestroyTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FieldDestroy();
+    }
+
+    private void FieldDestroy()
+    { 
+        DestroyTime -= Time.deltaTime;
+        if(DestroyTime < 0)
+        {
+            Destroy(gameObject);
+        }   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}
+public enum FieldType
+{
+    None,
+    Search,
+    invisible
+}
--- DTKunity1week_2026_3/Assets/Scripts/Ochiai_itemEffect_Script.cs
+++ DTKunity1week_2026_3/Assets/Scripts/Ochiai_itemEffect_Script.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Ochiai_ItemEffect_Script : MonoBehaviour
 {
     [Header("このオブジェクトのアイテムの種類")]
-    [SerializeField] private HangingItems thisItem; 
+    [SerializeField] private HangingItems thisItem;
+    [SerializeField] private GameObject field;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,17 +30,44 @@ public class Ochiai_ItemEffect_Script : MonoBehaviour
                 SmokeEffect();
                 break;
         }
+        Destroy(gameObject);
     }
 
-    //石が着弾したときの処理の関数
+    //石が着弾したときに実行する関数
     private void StoneEffect()
     {
+        Vector3 rayPos = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
+        Ray ray = new Ray(rayPos, -transform.up);
+        RaycastHit[] hits = Physics.RaycastAll(ray, 100.0f);
 
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.gameObject.tag == "ground")
+            {
+                Vector3 genePos = new Vector3(hit.point.x, hit.point.y + 0.1f, hit.point.z);
+                GameObject searchObj  = Instantiate(field, genePos, field.transform.rotation);
+            }
+        }
     }
 
-    //けむり玉が着弾したときの処理の関数
+    //けむり玉が着弾したときに実行する関数
     private void SmokeEffect()
     {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit[] hits = Physics.RaycastAll(ray, 100.0f);
 
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.gameObject.tag == "ground")
+            {
+                Vector3 genePos = new Vector3(hit.point.x, hit.point.y + 0.1f, hit.point.z);
+                GameObject searchObj = Instantiate(field, genePos, field.transform.rotation);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        LandingEffect();
     }
 }
