1fd52e0759d0114a4b1ed57f44c536f98abd43c9
osato18 <otuy.nin.121011@gmail.com>
2026-03-20 05:25:43 +0900

    カメラ＆移動完成

--- DTKunity1week_2026_3/Assets/Scripts/PlayerManager.cs
+++ DTKunity1week_2026_3/Assets/Scripts/PlayerManager.cs
@@ -17,6 +17,9 @@ public class PlayerManager : MonoBehaviour
     [Header("カメラオブジェクト")]
     [SerializeField]private GameObject _cameraObj;
 
+    [Header("プレイヤーズオブジェクト")]
+    [SerializeField]private GameObject _playersObj;
+
     //WASD入力を受け取る
     private Vector2 _moveInput;
 
@@ -30,11 +33,17 @@ public class PlayerManager : MonoBehaviour
     }
     private void Update()
     {
-        Vector3 cameraForwardDir = (transform.position-_cameraObj.transform.position).normalized;
-        Vector3 cameraRightDir=Vector3.Cross(Vector3.up,cameraForwardDir).normalized;
+        Vector3 cameraForwardDir = transform.position-_cameraObj.transform.position;
+        cameraForwardDir.y=0;
+        cameraForwardDir.Normalize();
+        Vector3 cameraRightDir=Vector3.Cross(Vector3.up,cameraForwardDir);
+        cameraRightDir.y=0;
+        cameraRightDir.Normalize();
         Vector3 moveDir=cameraForwardDir*_moveInput.y+cameraRightDir*_moveInput.x;
         moveDir.y=0;
         _playerRb.linearVelocity =moveDir*_moveSpeed;
+        Debug.Log("moveDir"+moveDir);
+        _playersObj.transform.forward=moveDir;
     }
     private void OnMove(InputValue value)
     {
