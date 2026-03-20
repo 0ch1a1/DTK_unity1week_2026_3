d438e4e7f7d8e09c0d5748194b5e644cb1b2b5fa
0ch1a1@DTK <taishi.842aka@icloud.com>
2026-03-20 18:42:13 +0900

    effectOfItemForEnemy

--- DTKunity1week_2026_3/Assets/Scripts/Ochiai_EnemyMove_Script.cs
+++ DTKunity1week_2026_3/Assets/Scripts/Ochiai_EnemyMove_Script.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
+using UnityEngine.Splines;
+using Unity.Mathematics;
 
+[ExecuteAlways]
 public class Ochiai_EnemyMove_Script : MonoBehaviour
 {
+    [SerializeField] private  SplineContainer spline;
+    [SerializeField] private GameObject followObject;
+    private float t;
+    [SerializeField] private float distance;
+    [SerializeField] private float speed;
+    private float SplineLength;
+
+    public bool cautionFlag;
+
+    public Transform targetTrans;
+    private Transform startEnemyTrans;
+
+    private bool cationInitialize;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +29,40 @@ public class Ochiai_EnemyMove_Script : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void MoveForCaution()
+    {
+        if (!cationInitialize)
+        {
+            startEnemyTrans = transform;
+            cationInitialize = true;
+        }
+    }
+
+    private void MoveOnSpline()
+    {
+        //Splineや追従オブジェクトの失効などを検知してエラー防止
+        if (!spline || !followObject) return;
+        if (spline.CalculateLength() == 0f) return;
+
+        SplineLength = spline.CalculateLength();
+        distance += speed / Time.deltaTime;
+        t = distance / SplineLength;
+
+        //t値のクランプ
+        t = math.saturate(t);
+
+        // Splineの計算をする核心部分
+        spline[0].Evaluate(t, out float3 pos, out float3 tangent, out float3 up);
+
+        // 位置の反映
+        followObject.transform.position = (Vector3)pos;
+
+        // 回転の反映
+        if (math.any(tangent))
+        {
+            followObject.transform.rotation = Quaternion.LookRotation((Vector3)tangent, (Vector3)up);
+        }
     }
 }
